e961262b8fc4355d7c9d5327c4af590873ad3fe5
theklamayer <t.mayer@campus.tu-berlin.de>
2023-11-15 12:21:44 +0100

    count_move_per_time function

--- train_def.py
+++ train_def.py
@@ -23,30 +23,29 @@ def count_moves(row, counts, index):
                     counts[j][index] += 1
 
 
-def count_move_per_time(row, counts, index, time_interval):
-    found_time_interval = False
+def count_move_per_time(row, counts, row_index, time_interval, ti_index):
+    base_index = ti_index*13
 
     for i in range(1, 2564):
         move = row["Move "+ str(i)]
-
-        if found_time_interval:
+        
+        if move == f't{time_interval}':
+            return;
+        
+        else:
             # Count actions for the given time interval
             if move == 's':
-                counts[10][index] += 1
+                counts[base_index+10][row_index] += 1
             elif move == 'Base':
-                counts[11][index] += 1
+                counts[base_index+11][row_index] += 1
             elif move == 'SingleMineral':
-                counts[12][index] += 1
+                counts[base_index+12][row_index] += 1
 
             # Count hotkeys for the given time interval
             elif isinstance(move, str):
                 for j in range(10):
                     if move.startswith(f"hotkey{j}_t{time_interval}"):
-                        counts[j][index] += 1
-
-        # Check if the current action is the target time interval
-        elif move == f't{time_interval}':
-            found_time_interval = True
+                        counts[base_index+j][row_index] += 1
 
 # Load the training dataset
 train_data = pd.read_csv('train_data.csv', delimiter=';')
@@ -61,34 +60,34 @@ train_data_new = train_data.iloc[:, :2]
 # add the count of Moves per row
 
 # new lists of counts
-counts = [[0] * 3052 for _ in range(68)]
+counts = [[0] * 3052 for _ in range(65)]
 
 # Specify the target time intervals
 time_intervals = [20, 60, 100, 200]
 
 # go through the rows
-for index, row in train_data.iterrows():
-    count_moves(row, counts, index)
+for row_index, row in train_data.iterrows():
+    count_moves(row, counts, row_index)
 
-    for i, time_interval in enumerate(time_intervals):
-        count_move_per_time(row, counts, index, time_interval)
+    for ti_index, time_interval in enumerate(time_intervals):
+        count_move_per_time(row, counts, row_index, time_interval, ti_index+1)
 
 
 for i in range(10):
-    train_data_new['hk' + str(i) + 'Counts'] = counts[i]
-
-for i, time_interval in enumerate(time_intervals):
-        base_index = 13 * i
-        for j in range(10):
-            train_data_new[f'hk{j}_t{time_interval}_Counts'] = counts[base_index + j]
-
-        train_data_new[f's_t{time_interval}_Counts'] = counts[base_index + 10]
-        train_data_new[f'base_t{time_interval}_Counts'] = counts[base_index + 11]
-        train_data_new[f'singleMineral_t{time_interval}_Counts'] = counts[base_index + 12]
-
+    train_data_new[f'hk{i}Counts'] = counts[i]
+    
 train_data_new['sCounts'] = counts[10]
 train_data_new['baseCounts'] = counts[11]
 train_data_new['singleMineralCounts'] = counts[12]
+
+for ti_index, time_interval in enumerate(time_intervals):
+    base_index = (ti_index+1)*13
+    for j in range(10):
+        train_data_new[f'hk{j}_t{time_interval}_Counts'] = counts[base_index + j]
+
+    train_data_new[f's_t{time_interval}_Counts'] = counts[base_index + 10]
+    train_data_new[f'base_t{time_interval}_Counts'] = counts[base_index + 11]
+    train_data_new[f'singleMineral_t{time_interval}_Counts'] = counts[base_index + 12]
 
 train_data_new.to_csv('actiontype_count.csv', index=False)
 
